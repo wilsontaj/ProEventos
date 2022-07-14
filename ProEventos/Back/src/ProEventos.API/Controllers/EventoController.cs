dcafb060113f3baa5f851e6a1182a8f11c6932a5
wilsontaj <wilsonjr@live.com>
2022-07-14 09:43:03 -0300

    Nova versão

---
+++ ProEventos/Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "value";
+        }
+
+        [HttpPut("{id}")]
+        public string Post(int id)
+        {
+            return $"Exemplo de PUT com id = {id}";
+        }
+    }
+}
