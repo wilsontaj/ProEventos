c159474b7ed46a912e05cdcaeb6ad525093e1511
wilsontaj <wilsonjr@live.com>
2022-07-26 23:13:12 -0300

    Adicionado backend

---
+++ ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos: any = [
+    {
+    Tema: 'Angular 11',
+    Local: 'Belo Horizonte'
+    },
+    {
+      Tema: '.NET 5',
+      Local: 'São Paulo'
+    },
+    {
+        Tema: 'Angular e suas novidades',
+        Local: 'Rio de Janeiro'
+    }
+]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
